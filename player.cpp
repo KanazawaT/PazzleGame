b8350136c3f8b7558be30d89ecd145aed6b764a6
LAPTOP-I16IFVFC\hinaka <e1c20012@st.oit.ac.jp>
2021-09-03 15:42:12 +0900

    draw

--- player.cpp
+++ player.cpp
@@ -10,5 +10,13 @@ void Player::move() {//キーボードorコントローラ入力読んでプレイヤー移動
 }
 
 void Player::draw() {//表示(ScreenFlip不要)
+	int charSize = 5;
+
+	int x =this->pos.getX();
+	int y = this->pos.getY();
+
+	DrawCircle(x, y, charSize, GetColor(255, 255, 255), 1);
+
 	//ScreenFlip();//裏画面の表示を表に持ってくる。これがないと表示されないがこの関数では必要ないのでデバッグ用に使って詳しくはググって
+
 }

71da2c4312443b353aa491698fd8e023936981f2
KanazawaT <e1q19024@oit.ac.jp>
2021-09-03 14:10:51 +0900

    player.cppをちょっと修正

--- player.cpp
+++ player.cpp
@@ -1,4 +1,5 @@
 #include "player.h"
+#include "DxLib.h"
 
 void Player::setPoint(int x, int y) {//プレイヤーの座標初期化
 
@@ -9,5 +10,5 @@ void Player::move() {//キーボードorコントローラ入力読んでプレイヤー移動
 }
 
 void Player::draw() {//表示(ScreenFlip不要)
-
+	//ScreenFlip();//裏画面の表示を表に持ってくる。これがないと表示されないがこの関数では必要ないのでデバッグ用に使って詳しくはググって
 }
